ba1ea82cad8f6b287f1db32cdcd924c5fa759115
Yue You <yue_you@berkeley.edu>
2016-11-21 19:23:33 -0800

    update plot.R

--- R/plot.R
+++ R/plot.R
@@ -1,31 +1,32 @@
-plot.mfa <- function(x, ...) {
-################## eigenvalues #####################
-  pdf("eigen value.pdf")
-  barplot(x$eigen, main = 'Eigenvalues', xlab = 'Components', ylab = "Eigenvalue of Component")
-  dev.off()
-############### common factor scores ####################
+plot.mfa <- function(x, dim_plot = c(1,2), cex = 0.8, ...) {
+  # check plotted dimensions
+  for (i in 1:2) {
+    if (dim_plot[i] < 0 || dim_plot[i] > 12) {
+      warning("wrong dimesions!\nSet dimesions as default: 1 & 2")
+    }
+  }
+##################set labels for factor plots###############################
+  factor_text <- NULL
   size_subsample <- dim(x$common_factor_scores)[1] / 3
   for (i in 1: dim(x$common_factor_scores)[1]) {
     if (i <= size_subsample) factor_text = c(factor_text, paste('NZ',i))
     if (i > size_subsample && i <= 2 * size_subsample) factor_text = c(factor_text, paste('FR',i - 4))
     if (i > 2 * size_subsample) factor_text = c(factor_text, paste('CA',i - 8))
   }
-  pdf("Figure 2a.pdf")
-  plot(x$common_factor_scores[,1], x$common_factor_scores[,2], main = "Common Factor Scores",
-       xlab = "1", ylab = "2", bty = "l", type = "n", axes=FALSE)
-  text(x$common_factor_scores[,1], x$common_factor_scores[,2], labels = factor_text)
-  abline(h=0); abline(v=0)
-  dev.off()
+
 ################## PROCESS PARTIAL SCORES #####################
-# exact partial scores for each assessor
+# extract partial scores for each assessor
   partial_factor_scores <- attributes(x$partial_factor_scores)
   partial_factor_scores[[1]] <- NULL
 
 ################## PROCESS LOADINGS #####################
-# re-scale loadings to have a variance of 1st 2 eigen values
-  loadings <- cbind(x$loadings[,1] / sd(x$loadings[,1]) * sqrt(sqrt(x$eigen[1])), 
-                    x$loadings[,2] / sd(x$loadings[,2]) * sqrt(sqrt(x$eigen[2])))
-# seperate different assessors
+  # re-scale loadings to have a variance of 1st 2 eigen values
+  # re-scale to 1
+  Loadings_std <- sweep(x$loadings, 2, apply(x$loadings, 2, sd), "/")
+  # re-scale to eigen-value
+  # notice: dim(loadings) = 12, length(eigen) = 11, so we create eigen[12] = eigen[11]
+  loadings <- sweep(Loadings_std, 2, c(sqrt(x$eigen), sqrt(x$eigen)[length(x$eigen)]), "*")
+  # seperate different assessors
   loadings_list <- list(
     "1" = loadings[1:6,],
     "2" = loadings[7:12,],
@@ -45,7 +46,7 @@ plot.mfa <- function(x, ...) {
    "3"<- c("V1", "V2", "V3", "V4", "V9", "V10"),
    "4"<- c("V1", "V2", "V3", "V4", "V8"),
    "5"<- c("V1", "V2", "V3", "V4", "V11", "V12"),
-    "6"<- c("V1", "V2", "V3", "V4", "V13"),
+   "6"<- c("V1", "V2", "V3", "V4", "V13"),
    "7"<- c("V1", "V2", "V3", "V4"),
    "8"<- c("V1", "V2", "V3", "V4", "V14", "V15"),
    "9"<- c("V1", "V2", "V3", "V4", "V15"),
@@ -53,26 +54,64 @@ plot.mfa <- function(x, ...) {
   )
 
 ################## PLOT #####################
-  pdf("Figure3.pdf", width=20, height=10)
+###############PLOT common factor scores ####################
+  plot(x$common_factor_scores[,dim_plot[1]], x$common_factor_scores[,dim_plot[2]], main = "Common Factor Scores",
+       xlab = "1", ylab = "2", bty = "l", type = "n", las = 1,
+       xlim = c(min(x$common_factor_scores[,dim_plot[1]])-0.5, max(x$common_factor_scores[,dim_plot[1]])+0.5),
+       ylim = c(min(x$common_factor_scores[,dim_plot[2]]), max(x$common_factor_scores[,dim_plot[2]])))
+  text(x$common_factor_scores[,dim_plot[1]], x$common_factor_scores[,dim_plot[2]], labels = factor_text, cex = cex, col = "blue")
+  abline(h=0, v=0)
+  
+##############PLOT partial factor scores ONLY #####################
   # plot 10 graphs in one doc.
-  par(mfrow=c(2,5))
+  multi_graph <- par(mfrow=c(2,5))
   # plot partial factor scores
   for (i in 1: length(partial_factor_scores)) {    # index = order of assessor + 1 
-    plot(partial_factor_scores[[i]][,1], partial_factor_scores[[i]][,2],  
-         xlab = "", ylab = "", bty = "l", type = "n", asp = 2, axes = FALSE)
-    # x axis
-    axis(side = 1, pos = c(0,0), tick = TRUE, labels = FALSE, lty = 1)
-   # y axis
-   axis(side = 2, pos = c(0,0), tick = TRUE, labels = FALSE, lty = 1)
-    # plot partial scores
-    text(partial_factor_scores[[i]][,1], partial_factor_scores[[i]][,2], labels = factor_text, cex = 0.8,
-          col = "blue")
-    # plot loadings
-    text(loadings_list[[i]][,1], loadings_list[[i]][,2], labels = loadings_text[[i]], col = "orange")
-   
-   mtext(side=1,text=paste0("Assessor", i), cex = 0.8)
+    plot(partial_factor_scores[[i]][,dim_plot[1]], partial_factor_scores[[i]][,dim_plot[2]], main = "Partial Factor Scores",
+         xlab = paste0("dim", dim_plot[1]), ylab = paste0("dim", dim_plot[2]), bty = "l", type = "n", las = 1,
+         xlim = c(min(partial_factor_scores[[i]][,dim_plot[1]])-0.5, max(partial_factor_scores[[i]][,dim_plot[1]])+0.5),
+         ylim = c(min(partial_factor_scores[[i]][,dim_plot[2]]), max(partial_factor_scores[[i]][,dim_plot[2]])))
+   # plot partial factor scores
+    text(partial_factor_scores[[i]][,dim_plot[1]], partial_factor_scores[[i]][,dim_plot[2]], cex = cex, 
+        labels = factor_text, col = "blue")
+    mtext(side=1,text=paste0("Assessor", i), cex = cex)
+    abline(h = 0, v = 0)
   }
-  dev.off()
-}
+  par(multi_graph)
 ##############################################
+##############PLOT loadings ONLY#####################
+  # plot 10 graphs in one doc.
+  multi_graph <- par(mfrow=c(2,5))
+  # plot partial factor scores
+  for (i in 1: length(partial_factor_scores)) {    # index = order of assessor + 1 
+    plot(partial_factor_scores[[i]][,dim_plot[1]], partial_factor_scores[[i]][,dim_plot[2]], main = "Partial Factor Scores",
+         xlab = paste0("dim", dim_plot[1]), ylab = paste0("dim", dim_plot[2]), bty = "l", type = "n", las = 1,
+         xlim = c(min(partial_factor_scores[[i]][,dim_plot[1]])-0.5, max(partial_factor_scores[[i]][,dim_plot[1]])+0.5),
+         ylim = c(min(partial_factor_scores[[i]][,dim_plot[2]]), max(partial_factor_scores[[i]][,dim_plot[2]])))
+    # plot loadings
+    text(loadings_list[[i]][,dim_plot[1]], loadings_list[[i]][,dim_plot[2]], labels = loadings_text[[i]], col = "orange")
+    mtext(side=1,text=paste0("Assessor", i), cex = cex)
+    abline(h = 0, v = 0)
+  }
+  par(multi_graph)
+##############PLOT partial factor scores && Loading TOGETHER#####################
+  # plot 10 graphs in one doc.
+  multi_graph <- par(mfrow=c(2,5))
+  # plot partial factor scores
+  for (i in 1: length(partial_factor_scores)) {    # index = order of assessor + 1 
+    plot(partial_factor_scores[[i]][,dim_plot[1]], partial_factor_scores[[i]][,dim_plot[2]], main = "Partial Factor Scores",
+         xlab = paste0("dim", dim_plot[1]), ylab = paste0("dim", dim_plot[2]), bty = "l", type = "n", las = 1,
+         xlim = c(min(partial_factor_scores[[i]][,dim_plot[1]])-0.5, max(partial_factor_scores[[i]][,dim_plot[1]])+0.5),
+         ylim = c(min(partial_factor_scores[[i]][,dim_plot[2]]), max(partial_factor_scores[[i]][,dim_plot[2]])))
+    # plot partial factor scores
+    text(partial_factor_scores[[i]][,dim_plot[1]], partial_factor_scores[[i]][,dim_plot[2]], cex = cex, 
+         labels = factor_text, col = "blue")
+    # plot loadings
+    text(loadings_list[[i]][,dim_plot[1]], loadings_list[[i]][,dim_plot[2]], labels = loadings_text[[i]], col = "orange")
+    mtext(side=1,text=paste0("Assessor", i), cex = cex)
+    abline(h = 0, v = 0)
+  }
+  par(multi_graph)
+}
+
 

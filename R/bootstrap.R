fe45849a60112b0c88e3081f5d9144644c3b362b
Xin Wang <imwangxin@gmail.com>
2016-11-21 18:39:27 -0800

    bootstrap with ncomp=12

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -9,14 +9,16 @@
 
 bootstrap.mfa <- function(X){
 #FS: factor scores of the initial data tablesi
-K=10
+K=10.0
 #sample with replacement
 B=sample(1:10, 10, replace=TRUE)
 #compute the bootstrapped estimate of the factor scores
 PFS = attributes(X$partial_factor_scores)
 PFS[[1]]=NULL
-sum = matrix(data=0, nrow=12, ncol=2)
-print(sum)
-print(PFS[[1]])
-FS_B = 1/10*(lapply(B, FUN = function(i) sum+PFS[[i]]))
+FS_B = matrix(data=0, nrow=12, ncol=12)
+for (i in 1:length(B)){
+  FS_B = FS_B + PFS[[B[i]]]
+}
+FS_B = 1/K*FS_B
+
 }

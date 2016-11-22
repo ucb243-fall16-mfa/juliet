a416d235ee17b06044b012271d41b460e3fd2f23
Yue You <yue_you@berkeley.edu>
2016-11-21 19:28:31 -0800

    update mfa_test1.R

--- mfa_test1.R
+++ mfa_test1.R
@@ -1,51 +1,10 @@
-<<<<<<< Updated upstream
-#Read the data from file(You may change the path)
-data_raw<-read.csv("D:/stat/wines.csv")
-
-#Pick the useful part
-data<-data_raw[,2:54]
-
-# print method
-print.mfa(review)
-# plot method
-plot.mfa(review)
-
-#Make a list store the block division
-pick1<-list(seq(1,6),seq(7,12),seq(13,18),seq(19,23),seq(24,29),
-            seq(30,34),seq(35,38),seq(39,44),seq(45,49),seq(50,53))
-
-col_name<-names(data)
-
-pick2<-list(col_name[1:6],col_name[7:12],col_name[13:18],col_name[19:23],col_name[24:29],
-            col_name[30:34],col_name[35:38],col_name[39:44],col_name[45:49],col_name[50:53])
-
-#Construct the object 
-review<-mfa1(data,pick1,scale=1)
-
-#Test the each meathods and functions
-#1.summary_eigen
-test1<-summary_eigen(review)
-
-#2.contribution
-test2<-contribution(review,type="observation")
-test3<-contribution(review,type="variable")
-test4<-contribution(review,type="table")
-
-#3.RV
-num_block<-length(review$weight)
-test5<-RV(review$scale_x[,seq(review$divide[1],review$divide[2]-1)],
-          review$scale_x[,seq(review$divide[num_block-1],review$divide[num_block]-1)])
-
-#4.LG
-test6<-LG(review$scale_x[,seq(review$divide[1],review$divide[2]-1)],
-          review$scale_x[,seq(review$divide[num_block-1],review$divide[num_block]-1)])
-=======
 #Read the data from file(You may change the path)
 data_raw<-read.csv("~/Github/stat-243-final/data/wines.csv")
 
 #Pick the useful part
 data<-data_raw[,2:54]
 
+
 #Make a list store the block division
 pick1<-list(seq(1,6),seq(7,12),seq(13,18),seq(19,23),seq(24,29),
             seq(30,34),seq(35,38),seq(39,44),seq(45,49),seq(50,53))
@@ -61,8 +20,7 @@ review<-mfa1(data,pick1,scale=1)
 # print method
 print.mfa(review)
 # plot method
-plot.mfa(review)
-
+plot.mfa(review, dim_plot = c(1,2), cex = 0.8)
 
 #Test the each meathods and functions
 #1.summary_eigen
@@ -81,4 +39,3 @@ test5<-RV(review$scale_x[,seq(review$divide[1],review$divide[2]-1)],
 #4.LG
 test6<-LG(review$scale_x[,seq(review$divide[1],review$divide[2]-1)],
           review$scale_x[,seq(review$divide[num_block-1],review$divide[num_block]-1)])
->>>>>>> Stashed changes

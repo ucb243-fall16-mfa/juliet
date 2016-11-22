f2cf7311f48dd387559a948b01535e0a9e480c26
Xin Wang <imwangxin@gmail.com>
2016-11-22 11:12:08 -0800

    added bootstrap function

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -1,24 +1,37 @@
 #' @title bootstrap.mfa
 #' @description a brief description
 #' @param X: a mfa object
+#' @param L: number of bootstrap samples are computed
 #' @export
-#' @return result:...
+#' @return bootstrap ratio
 #' @examples \dontrun{
 #' an example}
 
 
-bootstrap.mfa <- function(X){
+bootstrap.mfa <- function(X, L){
 #FS: factor scores of the initial data tablesi
-K=10.0
-#sample with replacement
-B=sample(1:10, 10, replace=TRUE)
+
 #compute the bootstrapped estimate of the factor scores
 PFS = attributes(X$partial_factor_scores)
 PFS[[1]]=NULL
-FS_B = matrix(data=0, nrow=12, ncol=12)
-for (i in 1:length(B)){
-  FS_B = FS_B + PFS[[B[i]]]
+FSBs = replicate(L, fsb(PFS))
+#compute bootstrap ratios
+FSB.mean = apply(FSBs, 1:2, mean)
+FSB.sd = apply(FSBs, 1:2, sd)
+boostrap.ratio = FSB.mean/FSB.sd
 }
-FS_B = 1/K*FS_B
 
+
+fsb = function(PFS){
+  #compute bootstrap factor score from partial factore scores of the inital data
+  #with a random sequence B
+  #sample with replacement
+  B=sample(1:10, 10, replace=TRUE)
+  FS_B = matrix(data=0, nrow=12, ncol=ncol(PFS[[1]]))
+  for (i in 1:length(B)){
+    FS_B = FS_B + PFS[[B[i]]]
+  }
+  FS_B = 1/length(B)*FS_B
 }
+FSB = bootstrap.mfa(review, 10)
+FSB
